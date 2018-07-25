41d0d73f6907e2e49f20cb63390b5c2a76e9ad24
Paulo Freire <higor.dev.ifce@gmail.com>
2018-07-24 23:36:24 -0300

    listando personagens
    utilizando a chave e o link da API para trazer uma lista de personagens

--- js/controllers/meu-controller.js
+++ js/controllers/meu-controller.js
@@ -1,7 +1,16 @@
 angular.module('roteamento').controller('meuController',function($scope,$http,$location,HarryService){
-    //$scope.alunos = HarryService.getAlunos(); 
+    console.log(HarryService);
+    $scope.alunos = HarryService.getAlunos(); 
     $scope.getDados = function(){
-        console.log(HarryService);
-        $scope.alunos = HarryService.getAlunos();
-    };
+    $http.get('https://www.potterapi.com/v1/characters?key=$2a$10$WXowU3W/YhymnbHnobMNN.54.GWaVAg/je0tS0Gg9MW9BrZOjxy6i')
+      .then(function successCallback(response) {
+          console.table(response.data);
+          HarryService.setAlunos(response.data);
+          $scope.alunos = HarryService.getAlunos(); 
+          $location.path('/sobre');
+      },
+      function errorCallback(response) {
+          alert(response);
+    });
+    }
 });
--- js/services/harry-service.js
+++ js/services/harry-service.js
@@ -1,14 +1,7 @@
 angular.module('roteamento').factory("HarryService",function(){
     self.alunos = {};
     var _getAlunos = function(){
-      $http.get('https://www.potterapi.com/v1/characters?key=$2a$10$WXowU3W/YhymnbHnobMNN.54.GWaVAg/je0tS0Gg9MW9BrZOjxy6i')
-      .then(function successCallback(response) {
-          console.table(response.data);
-          return response.data;
-      },
-      function errorCallback(response) {
-          alert(response);
-    });
+      return self.alunos;
   }
     var _setAlunos = function(lista){
         self.alunos = lista;
